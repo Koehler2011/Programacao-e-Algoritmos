c1d4577012d76f33a56d5e93bf2762fae690bd00
Eduardo Koehler <okdudu02@gmail.com>
2026-06-02 11:35:41 -0300

    Update 4-10num.cpp

--- 2-Bim/Lista-1/4-10num.cpp
+++ 2-Bim/Lista-1/4-10num.cpp
@@ -1,23 +1,30 @@
-// Online C++ compiler to run C++ program online
 #include <iostream>
 using namespace std;
 
 int main() {
-   int maior, menor, num, num1;
-   cout << "Digite 10 numeros diferentes"<<endl;
-    for(int i=1; i<=10; i++){
-        cout << "Escreva o "<< i <<"° número! ";
+    int maior, menor, num;
+    
+    cout << "Digite 10 numeros diferentes:" << endl;
+    
+    for(int i = 1; i <= 10; i++){
+        cout << "Escreva o " << i << " o numero: ";
         cin >> num;
-    }
-    if (num > maior){
         
-        maior = num;
-        
+        if (i == 1) {
+            maior = num;
+            menor = num;
+        } else {
+            if (num > maior) {
+                maior = num;
+            }
+            if (num < menor) {
+                menor = num;
+            }
+        }
     }
-    if (num1 < menor){
-        menor = num1;
-    }
-    cout << "O maior numero e: "<<maior<<endl;
-    cout<<"O menor numero e: "<<menor<<endl;
+    
+    cout << "O maior numero e: " << maior << endl;
+    cout << "O menor numero e: " << menor << endl;
+    
     return 0;
 }
